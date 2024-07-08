8f9cc13e7ff5f0784218309c6dfb553639283de2
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-07-08 18:26:09 +0300

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Said Tapaev on 08.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Said Tapaev on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
